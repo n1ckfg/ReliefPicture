04f7d37fdc3d191946ada1ae9503de4cd1f3ad12
Nick Fox-Gieg <nick@fox-gieg.com>
2022-01-24 19:51:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -90,13 +90,14 @@ const updateIntervalAdjustment = 200; // per character in query
 const photoDoDebug = false;
 
 // allow cross-domain access (CORS)
+/*
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   return next();
 });
-
+*/
 if (!debug) {
     http.createServer(function(req, res) {
         res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
